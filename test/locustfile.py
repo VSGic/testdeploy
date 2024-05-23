66af04a8fa10e7bc7e3d1021bdc8c84fe5d90e2e
Vasily <Vasily@VSGic.local>
2024-05-23 07:59:25 -0500

    Update tests

--- test/locustfile.py
+++ test/locustfile.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, TaskSet, task, between
+from locust.stats import stats_printer, stats_history
 
 class UserBehavior(TaskSet):
     @task(1)
@@ -8,4 +9,3 @@ class UserBehavior(TaskSet):
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
     wait_time = between(1, 5)
-
--- test/test.sh
+++ test/test.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-LOCUST_USERS=1000
-LOCUST_HATCH_RATE=1000
-LOCUST_DURATION=180s
-LOCUST_HOST=http://192.168.1.67:5000
-LOCUST_RESULTS=/mnt/locust/locust_output
+LOCUST_USERS=500
+LOCUST_HATCH_RATE=500
+LOCUST_DURATION=120s
+LOCUST_HOST=http://app:5000
 
-docker run -v ./:/mnt/locust/ locustio/locust \
-  -f /mnt/locust/locustfile.py \
+  -f ./locustfile.py \
   --headless \
   -u $LOCUST_USERS \
   -r $LOCUST_HATCH_RATE \
   -t $LOCUST_DURATION \
-  --csv $LOCUST_RESULTS \
   --host $LOCUST_HOST
